67253834a1db49ce4c425cd2e351ce4572797f34
Nikos Mantis <nmantis@drn-engineering.com>
2019-10-15 14:00:43 +0300

    config init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthService } from './services/auth.service';
+import { AuthInterceptor } from './services/auth-interceptor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs/internal/observable/throwError';
+import { retry, catchError } from 'rxjs/operators';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { environment } from 'src/environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+
+})
+export class AuthService {
+  private baseUrl = environment.apiUrl
+  static isLoggedIn: Boolean;
+
+  set setLoggedIn(value: Boolean) {
+    AuthService.isLoggedIn = value;
+  }
+
+  get isLoggedInValue() {
+    return AuthService.isLoggedIn;
+  }
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ) { }
+
+  showError() {
+    this._snackBar.open('Wrong Username or Password.', 'Ok', {
+      duration: 3000,
+      verticalPosition: 'top',
+      panelClass: ['warning']
+    });
+  }
+  public login(usernameOrEmail: string, password: string) {
+    //console.log('i login service with : '+usernameOrEmail+' and '+ password)
+    return this.http.post<any>(this.baseUrl + '/api/auth/signin', { usernameOrEmail, password }, httpOptions)
+      .pipe(
+        retry(1),
+        catchError((error): any => {
+          this.showError()
+          return throwError(
+            'Something bad happened; please try again later.');
+        })
+      );
+  }
+
+  public loginStatusUpdate() {
+    //is called if login is successfull and updates the isLoggedIN boolean
+    try {
+      if (localStorage.getItem('jwt_token')) {
+        AuthService.isLoggedIn = true;
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  public logout() {
+    if (localStorage.getItem('jwt_token')) {
+      localStorage.removeItem('jwt_token')
+      AuthService.isLoggedIn = false;
+      this.router.navigateByUrl('/login')
+    } else {
+      alert('You are not Logged in!')
+    }
+  }
+}
