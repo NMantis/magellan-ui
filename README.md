eab4d418353e6a87d990f9793dc1175b3b2a05a8
Nikos Mantis <nmantis@drn-engineering.com>
2019-10-15 13:18:31 +0300

    renamed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Magelan
+# magellan
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.7.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'magelan'`, () => {
+  it(`should have as title 'Magellan'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('magelan');
+    expect(app.title).toEqual('Magellan');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to magelan!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to magellan!');
   });
 });
