0c77c22854fbbabd8212e5eabbe8e42e5b949c9f
Nikos Mantis <nmantis@drn-engineering.com>
2020-03-07 02:05:32 +0200

    animations in places

--- src/app/pages/first-contact/first-contact.component.ts
+++ src/app/pages/first-contact/first-contact.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate, query, stagger, animateChild } from '@angular/animations';
 
 @Component({
   selector: 'app-first-contact',
   templateUrl: './first-contact.component.html',
-  styleUrls: ['./first-contact.component.scss']
+  styleUrls: ['./first-contact.component.scss'],
+  animations: [
+    trigger('places', [
+      transition(':enter', [
+        style({ transform: 'scale(0.5)', opacity: 0 }),  // initial
+        animate('1s cubic-bezier(.8, -0.6, 0.2, 1.5)', 
+          style({ transform: 'scale(1)', opacity: 1 }))  // final
+      ])
+    ]),
+    trigger('list', [
+      transition(':enter', [
+        query('@places', stagger(300, animateChild()))
+      ]),
+    ])
+  ]
 })
 export class FirstContactComponent implements OnInit {
   places: Array<any> = [];
