fa7ab4e63b1091f7b7b0ae636d643d9417fc8098
Billzg13 <Billzg1234@gmail.com>
2020-09-30 13:04:27 +0300

    new url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiUrl: 'http://ec2-18-195-215-56.eu-central-1.compute.amazonaws.com',
+  //apiUrl: 'http://ec2-18-195-215-56.eu-central-1.compute.amazonaws.com',
+  apiUrl: 'http://api:8000',
   production: true
 };
